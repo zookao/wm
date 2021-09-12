258ecc93df32714ee45f79a0d3243b8669fe14b8
root <caozongchao@163.com>
2021-09-12 21:19:23 +0800

    .

--- d.php
+++ d.php
@@ -1,5 +1,8 @@
 <?php
 $url = urldecode($_GET['url']);
+if(!$url){
+    exit();
+}
 if(!strstr($url,'http')){
     $url = 'http:'.$url;
 }
@@ -11,12 +14,12 @@ file_put_contents($name,$fileres);
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods:GET');
 header('Access-Control-Allow-Headers:*');
-$file = fopen ($name,"rb");
-Header ("Content-type: application/octet-stream" );
-Header ("Accept-Ranges: bytes" );
-Header ("Accept-Length: " . filesize ($name));
-Header ("Content-Disposition: attachment; filename=".$name);
-echo fread ($file, filesize ($name));
-fclose ($file);
+$file = fopen($name,"rb");
+Header("Content-type: application/octet-stream");
+Header("Accept-Ranges: bytes");
+Header("Content-Length: ".filesize($name));
+Header("Content-Disposition: attachment; filename=".$name);
+echo fread($file, filesize($name));
+fclose($file);
 unlink($name);
-exit ();
+exit();
